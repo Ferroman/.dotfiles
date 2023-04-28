63504b676dc047f4e163ec6d4e296969d7d7cd49
Bohdan F <bohdan.f@affinidi.com>
2023-04-28 12:16:52 +0100

    feat: display blame on current line

--- .config/nvim/lua/custom/configs/overrides.lua
+++ .config/nvim/lua/custom/configs/overrides.lua
@@ -66,4 +66,9 @@ M.nvimtree = {
   },
 }
 
+M.gitsigns = {
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_formatter = '<author>, <summary> - <author_time:%Y-%m-%d>',
+}
+
 return M
