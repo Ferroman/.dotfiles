845cd702725b6fa36dec8cdd31f9944bfe3fc19b
Bohdan F <bohdan.f@affinidi.com>
2023-05-07 16:46:14 +0300

    fix: disable nvim tabs; use block for statusline

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -11,12 +11,18 @@ M.ui = {
   hl_override = highlights.override,
   hl_add = highlights.add,
 
-  -- statusline = {
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow (separators work only for "default" statusline theme;
     -- round and block will work for the minimal theme only)
-    -- separator_style = "default",
+    separator_style = "block",
     -- overriden_modules = nil,
-  -- },
+  },
+
+  -- disable custom tabs
+  tabufline = {
+    enabled = false
+  },
 }
 
 M.plugins = "custom.plugins"
