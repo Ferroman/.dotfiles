cfd0e9c02c6935245da998c7c2ef5f57830032c8
Bohdan F <bohdan.f@affinidi.com>
2023-05-31 11:49:39 +0300

    fix(neovim): correct format for guifont

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -3,7 +3,7 @@ vim.opt.swapfile = false -- no need for swapfile with autosave plugin
 vim.opt.spelllang = 'en_us'
 vim.opt.spell = true
 
-vim.opt.guifont = { "Iosevka", "h16" }
+vim.opt.guifont = { "Iosevka Term:h20" }
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
