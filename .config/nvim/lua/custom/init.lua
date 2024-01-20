5be93d82cbf0583801d925263c7d7f6f65fabbe5
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2024-01-20 19:38:38 +0200

    chore: remove comments

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -4,8 +4,4 @@ vim.opt.spelllang = 'en_us'
 vim.opt.spell = true
 
 vim.opt.guifont = { "Iosevka Term:h20" }
--- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+
