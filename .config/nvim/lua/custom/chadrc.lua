8b7d9bea167641e75ab3edc5290b8a481f8f4163
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2024-01-20 19:37:57 +0200

    feat: change nvim status line

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -12,11 +12,7 @@ M.ui = {
   hl_add = highlights.add,
 
   statusline = {
-    theme = "default", -- default/vscode/vscode_colored/minimal
-    -- default/round/block/arrow (separators work only for "default" statusline theme;
-    -- round and block will work for the minimal theme only)
-    separator_style = "block",
-    -- overriden_modules = nil,
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
   },
 
   -- disable custom tabs
