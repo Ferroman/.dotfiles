4a1c1e318c79b1bebf9bcae6f562078b1c40616b
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2024-01-21 15:06:27 +0200

    feat: add main installation script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./scripts/homebrew-install.sh
+brew bundle
+
+echo "Set fish as default shell"
+FILE="/etc/shells"
+if grep -q "fish" "$FILE"; then
+    echo "fish is already set"
+else
+    sudo echo "/home/linuxbrew/.linuxbrew/bin/fish" | tee /etc/shells
+    sudo chsh -s /home/linuxbrew/.linuxbrew/bin/fish "$USER"
+    chsh -s /home/linuxbrew/.linuxbrew/bin/fish
+fi
