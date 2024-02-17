c0e7f40fa46c8aa0adff32cad01417b43dc3106b
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2024-02-17 15:42:39 +0200

    fix: fish plugin installer

--- scripts/fish-install.sh
+++ scripts/fish-install.sh
@@ -1,6 +1,26 @@
-echo /usr/bin/fish | sudo tee -a /etc/shells
-chsh -s /usr/bin/fish
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-fisher install jorgebucaran/nvm.fish
-nvm install latest
-set -U nvm_default_version latest
+#!/bin/bash
+
+echo "Configure fish plugins"
+
+# Install Fisher if it's not already installed
+if ! fish -c 'type fisher' &>/dev/null; then
+    fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+fi
+
+PLUGINS=(
+"wfxr/forgit"
+"PatrickF1/fzf.fish"
+"ferroman/theme-bfrankovskyi"
+"franciscolourenco/done"
+"rstacruz/fish-asdf"
+"edc/bass"
+)
+
+for PLUGIN in "${PLUGINS[@]}"; do
+  # Check if the plugin is already installed before attempting to install
+  if ! fish -c "fisher list | grep -q $PLUGIN"; then
+    fish -c "fisher install $PLUGIN"
+  else
+    echo "$PLUGIN is already installed."
+  fi
+done
