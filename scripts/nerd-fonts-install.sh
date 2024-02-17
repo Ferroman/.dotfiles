2a273273bda59dccbe131d5538d3262d9e6f93ae
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2024-02-17 16:00:01 +0200

    fix: idempotent nerd fonts install

--- scripts/nerd-fonts-install.sh
+++ scripts/nerd-fonts-install.sh
@@ -1,9 +1,27 @@
- curl -Lo NerdFontsSymbolsOnly.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/NerdFontsSymbolsOnly.zip
- unzip NerdFontsSymbolsOnly.zip
+#!/bin/bash
 
-sudo mkdir /usr/local/share/fonts/nerd-fonts-symbols/
-sudo mv *.ttf /usr/local/share/fonts/nerd-fonts-symbols/.
+# Define the destination directory for the fonts
+fonts_dir="/usr/local/share/fonts/nerd-fonts-symbols"
 
-sudo fc-cache -fv
+# Check if the Nerd Fonts symbols are already installed
+if [ "$(ls -A ${fonts_dir})" ]; then
+    echo "Nerd Fonts symbols already installed."
+else
+    echo "Downloading Nerd Fonts symbols..."
+    curl -Lo NerdFontsSymbolsOnly.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/NerdFontsSymbolsOnly.zip
 
-rm *zip
+    # Extract the fonts
+    unzip NerdFontsSymbolsOnly.zip -d nerd-fonts-symbols
+
+    # Create the destination directory if it doesn't exist
+    sudo mkdir -p ${fonts_dir}
+
+    # Move the extracted fonts to the destination directory
+    sudo mv nerd-fonts-symbols/*.ttf ${fonts_dir}/.
+
+    # Clean up the downloaded and extracted files
+    rm -rf NerdFontsSymbolsOnly.zip nerd-fonts-symbols
+
+    echo "Updating font cache..."
+    sudo fc-cache -fv
+fi
