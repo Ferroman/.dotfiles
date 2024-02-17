cd85497e106a1cd5b5621350e31e16144bf1d672
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2024-02-17 15:53:29 +0200

    fix: idempotent homebrew install

--- scripts/homebrew-install.sh
+++ scripts/homebrew-install.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
 echo "Install Homebrew"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Check if Homebrew is already installed
+if ! command -v brew &>/dev/null; then
+    echo "Homebrew not found. Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew is already installed. Updating Homebrew..."
+    brew update
+fi
