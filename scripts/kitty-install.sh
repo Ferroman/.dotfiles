452d653f64845a00b1476da6bc865a3c446d48bb
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2024-02-17 15:25:59 +0200

    docs: echo process

--- scripts/kitty-install.sh
+++ scripts/kitty-install.sh
@@ -1,2 +1,4 @@
+echo "Set kitty as default terminal emulator"
+
 sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /home/bohdanf/.local/kitty.app/bin/kitty 5
 sudo update-alternatives --config x-terminal-emulator
