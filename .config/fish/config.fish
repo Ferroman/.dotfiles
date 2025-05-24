fdaf42b1585655cc250bd75517432f25aa95ffe5
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2025-05-24 21:52:22 +0300

    fix: update configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,11 @@
 # ~/.config/fish/config.fish
 if status is-interactive
   mise activate fish | source
+  atuin init fish | source
 else
   mise activate fish --shims | source
 end
 
 zoxide init fish | source
 /home/linuxbrew/.linuxbrew/bin/mise activate fish | source
+starship init fish | source
